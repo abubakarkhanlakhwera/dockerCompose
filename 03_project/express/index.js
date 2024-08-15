8405dcf60652429827dc53d447f36f2eee94bea8
lakhwera_001 <abubakarkhanlakhwara@gmail.com>
2024-08-15 20:45:07 +0500

    new commit

--- 03_project/express/index.js
+++ 03_project/express/index.js
@@ -1,39 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const client = redis.createClient({
+    url: 'redis://redis:6379'
+});
 
 const port = 8080;
+client.connect().then(() => {
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "/views"));
 
-app.set("veiw engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
-app.get("/",(req,res)=>{
-    res.render("home.ejs");
+    app.get("/", (req, res) => {
+        res.render("home.ejs");
+    });
+
+    app.get("/rollDice", (req, res) => {
+        let diceVal = Math.floor(Math.random() * 6) + 1;
+        res.render("rollDice.ejs", { diceVal });
+    });
+
+    app.get("/ig/:username", (req, res) => {
+        let { username } = req.params;
+        const instaData = require("./data.json");
+        const data = instaData[username];
+        if (data) {
+            res.render("instagram.ejs", { data });
+        } else {
+            res.status(404).render("error.ejs");
+        }
+    });
 });
 
-app.get("/rollDice",(req,res)=>{
-    let diceVal = Math.floor(Math.random()*6) + 1;
-    // res.render("rollDice.ejs",{num : diceVal});
-    // res.render("rollDice.ejs",{ diceVal : diceVal});
-    res.render("rollDice.ejs",{ diceVal});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
-
-app.get("/ig/:username", (req,res)=>{
-    // const followers = ['adam','bob','abc','steve']
-    let {username} = req.params;
-     const instaData = require("./data.json");
-     const data = instaData[username];
-     if(data){
-        res.render("instagram.ejs", {data :  instaData[username] });
-     }else{
-        res.render("error.ejs")
-     }
-    
-
-});
-
-app.listen(port, ()=>{
-    console.log(`listening ot port ${port}`);
-});
-
-
-
